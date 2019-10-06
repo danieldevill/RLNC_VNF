917f3d20414be8c6ee8cd1ed279c615e3b615215
Daniel de Villiers <danieldevill@hotmail.com>
2019-10-06 21:37:16 +0200

    Added coder vnf dpdk functions

---
+++ coder/netconfig/setup_all.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Start DPDK setup.
+
+#Export $RTE_SDK
+export RTE_SDK=/usr/src/dpdk-stable-18.11.1
+
+#Bind device to DPDK
+echo Binding devices to DPDK..
+sudo ip link set ens4 down
+
+#Enable PMD
+sudo modprobe uio_pci_generic
+#sudo modprobe igb_uio
+#sudo insmod /usr/src/dpdk-stable-18.11.1/build/kmod/igb_uio.ko
+#sudo modprobe igb_uio
+
+#Bind NICs to DPDK
+sudo $RTE_SDK/usertools/dpdk-devbind.py --bind=uio_pci_generic ens4
+
+#Print outputs of dpdk drivers and ovs-vsctl/ovs-ofctl to confirm.
+sudo $RTE_SDK/usertools/dpdk-devbind.py -s
+
+#Print number of Hugepages
+grep Huge /proc/meminfo
